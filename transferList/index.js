d55c5fc35ecfbee6d6a1b5f2b0cbbc4b2d1d2240
kranthi1818 <kranthikumar1018@gmail.com>
2025-02-27 21:52:00 +0530

    --noedit

--- transferList/index.js
+++ transferList/index.js
@@ -2,46 +2,99 @@ let leftContainer = document.querySelector(".first-container")
 let buttonsContainer = document.querySelector(".second-container")
 let rightContainer = document.querySelector(".third-container")
 
-let leftElements = document.querySelectorAll(".left-elements")
-let rightElements = document.querySelectorAll(".right-elements")
+let singleElement = document.querySelectorAll('.single-element')
 
+let input = document.querySelectorAll('input')
 
-let input = document.querySelectorAll("input")
+let leftBtn = document.querySelector('.left')
+let rightBtn = document.querySelector('.right')
 
-buttonsContainer.addEventListener("click", function (event) {
+leftBtn.disabled = true;
+rightBtn.disabled = true;
 
+buttonsContainer.addEventListener('click',function(event){
+    
+    if(event.target.tagName != 'BUTTON') return
 
-     if (event.target.tagName !== 'BUTTON') return;
+    if(event.target.classList.contains('all-left')){
+        allLeft()
+    }else if(event.target.classList.contains('all-right')){
+        allRight()
+    }else if(event.target.classList.contains('left')){
+        left()
+    }else if(event.target.classList.contains('right')){
+        right()
+    }
+})
 
-  if (event.target.classList.contains("all-left")) {
+function allLeft(){
+     singleElement.forEach((ele)=>{
+        leftContainer.append(ele)
+     })
+}
 
-    leftContainer.append(...rightElements, ...leftElements)
+function allRight(){
+    singleElement.forEach((ele)=>{
+        rightContainer.append(ele)
+     })
+}
 
-
-
-  } else if (event.target.classList.contains("all-right")) {
-
-    rightContainer.append(...rightElements, ...leftElements)
-
-   
-
-  }else if(event.target.classList.contains("left")){
-
-
-    input.forEach((inp)=>{
-        if(inp.checked == true){
-            leftContainer.append(inp.parentElement)
+function left(){
+    rightContainer.querySelectorAll('input').forEach((inp)=>{
+        if(inp.checked){
+             leftContainer.append(inp.parentElement)
+             inp.checked = false
         }
     })
-       
+}
 
-  }else if(event.target.classList.contains("right")){
-    input.forEach((inp)=>{
-        if(inp.checked == true){
-            rightContainer.append(inp.parentElement)
+function right(){
+    leftContainer.querySelectorAll('input').forEach((inp)=>{
+        if(inp.checked){
+             rightContainer.append(inp.parentElement)
+             inp.checked = false
         }
     })
-        
-  }
-  input.forEach((inp)=>inp.checked = false)
+}
+
+function updateBtn() {
+    let leftCheckboxes = leftContainer.querySelectorAll("input");
+    let leftChecked = false;
+
+    for (let i = 0; i < leftCheckboxes.length; i++) {
+        if (leftCheckboxes[i].checked) {
+            leftChecked = true;
+            break;
+        }
+    }
+
+    if (leftChecked) {
+        rightBtn.disabled = false;
+    } else {
+        rightBtn.disabled = true;
+    }
+
+    
+    let rightCheckboxes = rightContainer.querySelectorAll("input");
+    let rightChecked = false;
+
+    for (let i = 0; i < rightCheckboxes.length; i++) {
+        if (rightCheckboxes[i].checked) {
+            rightChecked = true;
+            break;
+        }
+    }
+
+    if (rightChecked) {
+        leftBtn.disabled = false;
+    } else {
+        leftBtn.disabled = true;
+    }
+}
+
+document.addEventListener("change", function (event) {
+    if (event.target.type === "checkbox") {
+        updateBtn();
+        console.log('hi')
+    }
 })
